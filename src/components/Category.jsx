1f0ac1f7c116c676bded8ddcdb82c096bc053395
Kenneth Murray <kwmwins@gmail.com>
2023-10-18 18:36:41 -0700

    more changes

---
+++ src/components/Category.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { categories } from "../data/data";
+
+const Category = () => {
+    console.log(categories)
+    return (
+        <div>
+            <h1>Top Rated Menu Items</h1>
+            {/* Categories */}
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Category;
--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -15,7 +15,14 @@ const filterType = (category) => {
         );
 };
 
-
+// Filter by price
+const filterPrice = (price) => {
+    setFoods(
+        data.filter((item) => {
+            return item.price === price;
+        })
+    );
+};
 
     return (
         <div className="max-w-[1640px] m-auto px-4 py-12">
@@ -28,21 +35,21 @@ const filterType = (category) => {
                 <div>
                    <p className="font-bold text-gray-700">Filter Type</p>
                    <div className="flex justify-between flex-wrap">
-                    <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">All</button>
-                    <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Burger</button>
-                    <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Pizza</button>
-                    <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Salads</button>
-                    <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Chicken</button>
+                    <button onClick={()=> setFoods(data)} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">All</button>
+                    <button onClick={()=> filterType('burger')} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Burger</button>
+                    <button onClick={()=> filterType('pizza')} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Pizza</button>
+                    <button onClick={()=> filterType('salad')} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Salads</button>
+                    <button onClick={()=> filterType('chicken')} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">Chicken</button>
                    </div>
                 </div>
                 {/*Filter Price*/}
                 <div>
                     <p className="font-bold text-gray-700">Filter Price</p>
                     <div className="flex justify-between max-w-[390px] w-full">
-                        <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$</button>
-                        <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$</button>
-                        <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$$</button>
-                        <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$$$</button>
+                        <button onClick={()=> filterPrice('$')} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$</button>
+                        <button onClick={()=> filterPrice('$$')} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$</button>
+                        <button onClick={()=> filterPrice('$$$')} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$$</button>
+                        <button onClick={()=> filterPrice('$$$$')} className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white">$$$$</button>
                     </div>
                 </div>
             </div>
